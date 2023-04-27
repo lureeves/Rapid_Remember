d2c12739eed59860360a40a3a4ad08c449f5ecc7
icncode <ljr77777@gmail.com>
2023-04-26 21:43:55 -0700

    Added documentation to beginner file

--- app/static/js/beginner.js
+++ app/static/js/beginner.js
@@ -1,74 +1,82 @@
+// Get the input field and text element from the HTML document
 const inputField = document.getElementById('input-field');
 let text = document.getElementById('text');
+
+// If the text element exists, process the text
 if (text) {
+  // Convert the text to lower case, remove punctuation, and split it into an array of words
   text = text.textContent.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").split(' ');
 }
-let score = 0; // Tracks how few helps user needed, higher is better
-const textArray = text.filter(emptyWord => emptyWord !== '') // Removes empty elements
+
+// Initialize variables to track score, current word index, and current word
+let score = 0;
+const textArray = text.filter(emptyWord => emptyWord !== '');
 let currentWordIndex = 0;
 let currentWord = textArray[currentWordIndex];
+
+// Set the value of the input field to an empty string and give it focus
 inputField.value = '';
 inputField.focus();
 
+// Add an event listener for when a key is pressed in the input field
 inputField.addEventListener('keydown', (event) => {
-    if (event.key === ' ') {
-        event.preventDefault(); // prevent default spacebar behavior
-        const userInput = inputField.value.trim().toLowerCase();
+  // If the key pressed is the spacebar, prevent the default behavior and continue
+  if (event.key === ' ') {
+    event.preventDefault();
 
-        // Increment/decrement if the word entered is the next in the sequence
-        if (userInput === currentWord) {
-            score++; // Increment the score
-            currentWordIndex++; // Incremenet word index
+    // Get the user's input, trim it, and convert it to lower case
+    const userInput = inputField.value.trim().toLowerCase();
 
-            // Add the previous word to the display
-            const previousWord = textArray[currentWordIndex - 1];
-            const correctWords = document.getElementById('correctWords')
-            correctWords.textContent += previousWord + ' ';
+    // If the user entered the correct word, increment the score and move to the next word
+    if (userInput === currentWord) {
+      score++;
+      currentWordIndex++;
 
-            if (currentWordIndex < textArray.length) {
-                currentWord = textArray[currentWordIndex];
-                inputField.value = ''; // clear the input field
-            } else {
-                // User finished the text
-                inputField.value = '';
-                // Open modal
-                const modal = document.getElementById('myModal');
-                modal.style.display = 'block';
+      // Add the previous word to the "correct words" display element
+      const previousWord = textArray[currentWordIndex - 1];
+      const correctWords = document.getElementById('correctWords');
+      correctWords.textContent += previousWord + ' ';
 
-                // Display final score in modal body
-                const finalScore = document.getElementById('finalScore');
-                finalScore.textContent = 'Final Score: ' + score;
+      // If there are still more words, move to the next one and clear the input field
+      if (currentWordIndex < textArray.length) {
+        currentWord = textArray[currentWordIndex];
+        inputField.value = '';
+      } else {
+        // If there are no more words, display the final score in a modal dialog
+        inputField.value = '';
+        const modal = document.getElementById('myModal');
+        modal.style.display = 'block';
+        const finalScore = document.getElementById('finalScore');
+        finalScore.textContent = 'Final Score: ' + score;
 
-                // Practice Again Button
-                const practiceAgainButton = modal.querySelector('.btn-primary');
-                score = 0; // Resets score
-                currentWordIndex = 0; // Reset word index
-                practiceAgainButton.addEventListener('click', () => {
-                    correctWords.textContent += '';
-                    inputField.value = ''; // Clear input field
-                    modal.style.display = 'none'; // Close modal
-                    location.reload(); // Reload the page
-                    inputField.focus(); // Put cursor in text field
-                });
-
-
-                // My Texts Button
-                const playAgainButton = modal.querySelector('.btn-secondary');
-                playAgainButton.addEventListener('click', () => {
-                    window.location.href = '/'; // Redirect to home page
-                });
-            }
-        } else { // Wrong word entered
-            score--; // Decrement score
-
-            // flash the input field border red
-            inputField.style.backgroundColor = 'red';
-            setTimeout(() => {
-                inputField.style.backgroundColor = '';
-            }, 500); // set timeout to reset border color after 100 milliseconds
-        }
-        const scoreDisplay = document.getElementById('score');
-        scoreDisplay.textContent = score; // Display the score
-        inputField.value = '' // Clear input field
+        // Add event listeners to the "Practice Again" and "My Texts" buttons in the modal dialog
+        const practiceAgainButton = modal.querySelector('.btn-primary');
+        score = 0;
+        currentWordIndex = 0;
+        practiceAgainButton.addEventListener('click', () => {
+          correctWords.textContent += '';
+          inputField.value = '';
+          modal.style.display = 'none';
+          location.reload();
+          inputField.focus();
+        });
+        const playAgainButton = modal.querySelector('.btn-secondary');
+        playAgainButton.addEventListener('click', () => {
+          window.location.href = '/';
+        });
+      }
+    } else {
+      // If the user entered the wrong word, decrement the score and flash the input field red
+      score--;
+      inputField.style.backgroundColor = 'red';
+      setTimeout(() => {
+        inputField.style.backgroundColor = '';
+      }, 500);
     }
+
+    // Update the score display and clear the input field
+    const scoreDisplay = document.getElementById('score');
+    scoreDisplay.textContent = score;
+    inputField.value = '';
+  }
 });
